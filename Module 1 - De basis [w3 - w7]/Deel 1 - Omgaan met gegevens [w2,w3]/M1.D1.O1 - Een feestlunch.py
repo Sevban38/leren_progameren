c82e3d7d9a4727a8d7e4297e23453243f6759692
Sevban38 <sevbanburgaz078@outlook.com>
2024-09-09 11:25:56 +0200

    Create M1.D1.O1 - Een feestlunch.py

---
+++ Module 1 - De basis [w3 - w7]/Deel 1 - Omgaan met gegevens [w2,w3]/M1.D1.O1 - Een feestlunch.py
@@ -0,0 +1,17 @@
+
+croissantjes = 17
+croissant_price = 0.39
+
+
+stokbroden = 2
+stokbrood_price = 2.78
+
+
+kortingsbonnen = 3
+kortingsbon_value = 0.50
+
+
+total_cost = (croissantjes * croissant_price) + (stokbroden * stokbrood_price) - (kortingsbonnen * kortingsbon_value)
+
+
+print("The total amount to pay is:", total_cost, "euro")  
