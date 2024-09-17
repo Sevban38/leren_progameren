b7483c790903adc6421083e00dc5830bd5c60983
Sevban38 <sevbanburgaz078@outlook.com>
2024-09-17 22:47:54 +0200

    Create question.py

---
+++ Module 1 - De basis [w3 - w7]/Deel 1 - Omgaan met gegevens [w2,w3]/question.py
@@ -0,0 +1,8 @@
+#alle input vragen
+naam = input("wat is uw naam: ")
+leeftijd = input(f"goedendag {naam}, wat is je leeftijd: ")
+eten = input (f"en als {leeftijd} jarige, wat eet je graag: ")
+
+#print alles
+print("                                                         ")
+print(f"ah, {naam} van {leeftijd} jaar, eet graag {eten}.")
