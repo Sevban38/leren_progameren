659df783abb0d65725311dcdb4813c9eb0398667
Sevban38 <sevbanburgaz078@outlook.com>
2024-09-18 11:53:58 +0200

    Update M1.D1.O7 - Kom maar op met die invoer.py

--- Module 1 - De basis [w3 - w7]/Deel 1 - Omgaan met gegevens [w2,w3]/M1.D1.O7 - Kom maar op met die invoer.py
+++ Module 1 - De basis [w3 - w7]/Deel 1 - Omgaan met gegevens [w2,w3]/M1.D1.O7 - Kom maar op met die invoer.py
@@ -1,26 +1,26 @@
 croissantjes = int(input("Hoeveel croissantjes wil je kopen? "))
-croissant_price = float(input("Wat is de prijs van een croissantje? "))
+croissant_price = int(input("Wat is de prijs van een croissantje?"))
 
 stokbroden = int(input("Hoeveel stokbroden wil je kopen? "))
-stokbrood_price = float(input("Wat is de prijs van een stokbrood? "))
+stokbrood_price = int(input("Wat is de prijs van een stokbrood? "))
 
 kortingsbonnen = int(input("Hoeveel kortingsbonnen heb je? "))
-kortingsbon_value = float(input("Wat is de waarde van een kortingsbon? "))
+kortingsbon_value = int(input("Wat is de waarde van een kortingsbon? "))
 
-total_cost = (croissantjes * croissant_price) + (stokbroden * stokbrood_price) - (kortingsbonnen * kortingsbon_value)
+total_cost = (croissantjes * (croissant_price * 100)) + (stokbroden * (stokbrood_price * 100)) - (kortingsbonnen * (kortingsbon_value * 100))
 
-print(f"De totale prijs is: {total_cost} euro")
+print(f"De totale prijs is: {total_cost} eurocent.")
 
 
 
 vrienden = int(input("Hoeveel vrienden gaan er mee? "))
 
-ticket_kosten = float(input("Wat is de prijs van een ticket? "))
+ticket_kosten = int(input("Wat is de prijs van een ticket? "))
 
 spelduur = int(input("Hoeveel minuten duurt het spel? "))
 
-spel_kosten_per_minuut = float(input("Wat zijn de kosten per minuut voor het spel? ")) / 5
+spel_kosten_per_minuut = int(input("Wat zijn de kosten per minuut voor het spel? ")) / 5
 
-totale_kosten = (vrienden * ticket_kosten) + (vrienden * (spelduur / 5) * spel_kosten_per_minuut)
+totale_kosten = (vrienden * (ticket_kosten * 100)) + (vrienden * (spelduur / 5) * (spel_kosten_per_minuut*100))
 
-print(f"De totale kosten voor dit geweldige uitje naar de speelhal is: {totale_kosten} euro.")
+print(f"De totale kosten voor dit geweldige uitje naar de speelhal is: {totale_kosten} eurocent.")
