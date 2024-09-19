3da1fe6c64c42760bb0b7f6df878450cc39b1426
Sevban38 <sevbanburgaz078@outlook.com>
2024-09-19 18:40:30 +0200

    gastheer naam slb-er of student

--- Module 1 - De basis [w3 - w7]/Deel 2 - Wat als [w4]/startTheParty.py
+++ Module 1 - De basis [w3 - w7]/Deel 2 - Wat als [w4]/startTheParty.py
@@ -1,14 +1,22 @@
+# Vraag de gebruiker om de naam van de gastheer
+gastheer = input("Wie is de gastheer? ")
 
-gastheer = True
+# namen van student (ik) en slb'er
+mijn_naam = "Sevban"
+slb_naam = "Eugene"
+
+# Controleer of er een gastheer is
+is_gastheer = bool(gastheer)
+
 gasten = True
 drank = True
 chips = True
 
 # Een feest moet minimaal gasten of een gastheer hebben
-start_condition_1 = gastheer or gasten
+start_condition_1 = is_gastheer or gasten
 
 # Het feest kan alleen beginnen als de gastheer er is tenzij er gasten chips en drank zijn
-start_condition_2 = gastheer or (gasten and chips and drank)
+start_condition_2 = is_gastheer or (gasten and chips and drank)
 
 # Een feest met chips maar zonder drank kan niet beginnen
 start_condition_3 = not (chips and not drank)
@@ -17,13 +25,18 @@ start_condition_3 = not (chips and not drank)
 start_condition_4 = not (gasten and not (chips or drank))
 
 # Een gastheer kan een feest geven zonder chips en gasten maar niet zonder drank
-start_condition_5 = not (gastheer and not drank)
+start_condition_5 = not (is_gastheer and not drank)
 
 # Alleen chips is geen feest
-start_condition_6 = not (chips and not (gastheer or gasten or drank))
+start_condition_6 = not (chips and not (is_gastheer or gasten or drank))
 
-if (start_condition_1 and start_condition_2 and start_condition_3 and 
-    start_condition_4 and start_condition_5 and start_condition_6):
+# Controleer naam gastheer
+special_condition_1 = gastheer == mijn_naam
+special_condition_2 = gastheer == slb_naam
+
+if (special_condition_1 or 
+    (not special_condition_2 and start_condition_1 and start_condition_2 and 
+     start_condition_3 and start_condition_4 and start_condition_5 and start_condition_6)):
     print('Start the Party')
 else:
     print('No Party')
