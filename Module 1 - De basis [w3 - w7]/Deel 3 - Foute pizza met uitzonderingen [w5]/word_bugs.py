25a928e4986cae79a03be514de31c89da81c3abf
Sevban38 <sevbanburgaz078@outlook.com>
2024-09-30 11:59:30 +0200

    Update word_bugs.py

--- Module 1 - De basis [w3 - w7]/Deel 3 - Foute pizza met uitzonderingen [w5]/word_bugs.py
+++ Module 1 - De basis [w3 - w7]/Deel 3 - Foute pizza met uitzonderingen [w5]/word_bugs.py
@@ -1,9 +1,9 @@
 a = str(input('Geef een woord: '))
 b = str(input('Geef nog een woord: '))
 
-if a > b:     
+if len(a) > len(b):     
     print('Woord 2 heeft meer letters dan woord 1')
-elif a < b:   
+elif len(a) < len(b):   
     print('Woord 2 heeft minder letters dan woord 1')
-else:                   
+elif len(a) == len(b):                   
     print('Woord 1 en woord 2 hebben evenveel letters')
