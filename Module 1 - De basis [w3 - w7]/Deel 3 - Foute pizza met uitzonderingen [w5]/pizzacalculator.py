451549eee16c5f54552363aaa3793d45fb499f06
Sevban38 <sevbanburgaz078@outlook.com>
2024-09-30 11:43:41 +0200

    gefixed

--- Module 1 - De basis [w3 - w7]/Deel 3 - Foute pizza met uitzonderingen [w5]/pizzacalculator.py
+++ Module 1 - De basis [w3 - w7]/Deel 3 - Foute pizza met uitzonderingen [w5]/pizzacalculator.py
@@ -1,9 +1,12 @@
 def get_pizza_count(size, price):
     while True:
-        count = input(f'Hoeveel {size} pizzas wilt u? ({price}$) ')
-        if count.isdigit():
-            return int(count)
-        else:
+        try:
+            count = float(input(f'Hoeveel {size} pizzas wilt u? ({price}$) '))
+            if count.is_integer() and count >= 0:
+                return int(count)
+            else:
+                print("U heeft iets verkeerd ingevuld, probeer nogmaals.")
+        except ValueError:
             print("U heeft iets verkeerd ingevuld, probeer nogmaals.")
 
 def calculate_total(small_count, medium_count, large_count):
@@ -18,9 +21,14 @@ def calculate_total(small_count, medium_count, large_count):
     total = total_small + total_medium + total_large
 
     print('-------------------------')
-    print(f'   {total_small} voor small')
-    print(f'   {total_medium} voor medium')
-    print(f'   {total_large} voor large')
+    if total_small > 0:
+        print(f'   {total_small} voor small')
+    if total_medium > 0:
+        print(f'   {total_medium} voor medium')
+    if total_large > 0:
+        print(f'   {total_large} voor large')
+
+    print('-------------------------')
     print(f'   {total} euro totaal')
     print('-------------------------')
 
@@ -28,4 +36,4 @@ small_pizza = get_pizza_count('small', 7)
 medium_pizza = get_pizza_count('medium', 9.50)
 large_pizza = get_pizza_count('large', 12)
 
-calculate_total(small_pizza, medium_pizza, large_pizza) 
+calculate_total(small_pizza, medium_pizza, large_pizza)
--- Module 1 - De basis [w3 - w7]/Deel 3 - Foute pizza met uitzonderingen [w5]/word_bugs.py
+++ Module 1 - De basis [w3 - w7]/Deel 3 - Foute pizza met uitzonderingen [w5]/word_bugs.py
@@ -2,8 +2,8 @@ a = str(input('Geef een woord: '))
 b = str(input('Geef nog een woord: '))
 
 if a > b:     
-    print('Woord 1 heeft meer letters dan woord 2')
+    print('Woord 2 heeft meer letters dan woord 1')
 elif a < b:   
-    print('Woord 1 heeft minder letters dan woord 2')
+    print('Woord 2 heeft minder letters dan woord 1')
 else:                   
     print('Woord 1 en woord 2 hebben evenveel letters')
