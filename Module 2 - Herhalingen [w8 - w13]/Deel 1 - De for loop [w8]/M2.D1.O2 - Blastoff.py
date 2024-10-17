da4eed20f7ca3a899c6934287cf359aaaef5ea39
Sevban38 <sevbanburgaz078@outlook.com>
2024-10-18 00:58:35 +0200

    opdrachten gemaakt module 2

---
+++ Module 2 - Herhalingen [w8 - w13]/Deel 1 - De for loop [w8]/M2.D1.O2 - Blastoff.py
@@ -0,0 +1,6 @@
+import time
+
+for i in range(30, 0, -1):
+    print(i)
+    time.sleep(1)
+print("Lancering!")
---
+++ Module 2 - Herhalingen [w8 - w13]/Deel 1 - De for loop [w8]/M2.D1.O3 - de tijd.py
@@ -0,0 +1,3 @@
+for period in ['AM', 'PM']:
+    for hour in range(1, 13):
+        print(f"{hour} {period}")
