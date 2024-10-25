3e70ce4f8cbf23d30507642b4ab32a62b29cda01
Sevban38 <sevbanburgaz078@outlook.com>
2024-10-25 09:13:32 +0200

    meer gemaakt en bijgevoegd

--- Module 2 - Herhalingen [w8 - w13]/Deel 2 - Collections [w9]/M2.D2.O1 - (Dagen).py
+++ Module 2 - Herhalingen [w8 - w13]/Deel 2 - Collections [w9]/M2.D2.O1 - (Dagen).py
@@ -1,19 +1,30 @@
 
 dagen_van_de_week = ("maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag")
 
-
 print("De dagen van de week zijn:")
 for dag in dagen_van_de_week:
-    print(dag, end=", " if dag != "zondag" else ".\n")
+    print('-', dag)
 
-
-werkdagen = dagen_van_de_week[:5]
 print("\nDe werkdagen zijn:")
+werkdagen = dagen_van_de_week[:5]
 for dag in werkdagen:
-    print(dag, end=", " if dag != "vrijdag" else ".\n")
+    print(dag)
 
-
-weekend = dagen_van_de_week[5:]
 print("\nDe weekenddagen zijn:")
+weekend = dagen_van_de_week[5:]
 for dag in weekend:
-    print(dag, end=", " if dag != "zondag" else ".\n")
+    print(dag)
+
+print("\nDe dagen van de week omgekeerd:")
+for dag in reversed(dagen_van_de_week):
+    print(dag)
+
+print("\nDe werkdagen van vrijdag tot maandag zijn:")
+werkdagen_omgekeerd = dagen_van_de_week[4::-1]
+for dag in werkdagen_omgekeerd:
+    print(dag)
+
+print("\nDe weekenddagen omgekeerd zijn:")
+weekend_omgekeerd = dagen_van_de_week[6:4:-1]
+for dag in weekend_omgekeerd:
+    print(dag)
--- Module 2 - Herhalingen [w8 - w13]/Deel 2 - Collections [w9]/M2.D2.O3 - Deck van speelkaarten.py
+++ Module 2 - Herhalingen [w8 - w13]/Deel 2 - Collections [w9]/M2.D2.O3 - Deck van speelkaarten.py
@@ -1,7 +1,7 @@
 import random
 
-suits = ['Harten', 'Klaveren', 'Schoppen', 'Ruiten']
-ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Boer', 'Vrouw', 'Heer', 'Aas']
+suits = ['Harten', 'Klaveren', 'Schoppen', 'Ruiten', 'vierkanten']
+ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Boer', 'Vrouw', 'Heer', 'Aas', 'timmerman']
 deck = [f'{rank} van {suit}' for suit in suits for rank in ranks] + ['Joker', 'Joker']
 
 random.shuffle(deck)
