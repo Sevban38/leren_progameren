c6c5f2d59d8828e7672cc9f42a8afb36ae8befd1
Sevban38 <sevbanburgaz078@outlook.com>
2024-11-08 11:19:27 +0100

    Update M2.D2.O1 - (Dagen).py

--- Module 2 - Herhalingen [w8 - w13]/Deel 2 - Collections [w9]/M2.D2.O1 - (Dagen).py
+++ Module 2 - Herhalingen [w8 - w13]/Deel 2 - Collections [w9]/M2.D2.O1 - (Dagen).py
@@ -1,32 +1,21 @@
+weekdagen = ('maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag', 'zondag')
 
-dagen_van_de_week = ("maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag")
+print("Alle dagen van de week zijn:")
+for dag in (weekdagen):
+    print(f"- {dag}")
 
-print("De dagen van de week zijn:")
-for dag in dagen_van_de_week:
-    print('-', dag)
+print("\nDe werkdagen zijn: maandag, dinsdag, woensdag, donderdag & vrijdag.")
 
-print("\nDe werkdagen zijn:", end=" ")
-werkdagen = dagen_van_de_week[:5]
-for dag in werkdagen:
-    print(dag, end=" ")
-print ('\n')
-print("\nDe weekenddagen zijn:", end=" ")
-weekend = dagen_van_de_week[5:]
-for dag in weekend:
-    print(dag, end=" ")
-print ('\n')
-print("\nDe dagen van de week omgekeerd:", end=" ")
-for dag in reversed(dagen_van_de_week):
-    print(dag, end=" ")
-print ('\n')
+print("\nDe weekenddagen zijn: zaterdag & zondag.")
 
-print("\nDe werkdagen van vrijdag tot maandag zijn:")
-werkdagen_omgekeerd = dagen_van_de_week[4::-1]
-for dag in werkdagen_omgekeerd:
-    print(dag)
+print("\nAlle dagen in omgekeerde volgorde zijn:")
+print(" -> ".join(weekdagen[::-1]))
 
-print("\nDe weekenddagen omgekeerd zijn:", end=" ")
-weekend_omgekeerd = dagen_van_de_week[6:4:-1]
-for dag in weekend_omgekeerd:
-    print(dag, end=" ")
-print('\n')
+werkdagen = ('maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag')
+
+print("\nDe werkdagen in omgekeerde volgorde zijn:")
+for dag in reversed(werkdagen):
+    print(f"- {dag}")
+
+
+print("\nDe weekenddagen in omgekeerde volgorde zijn: zondag + zaterdag.")
