2ae7fffb13147670d131a83c94cdf13d16df0dfd
Sevban38 <sevbanburgaz078@outlook.com>
2024-11-26 10:40:59 +0100

    Create medium2.py

---
+++ Module 2 - Herhalingen [w8 - w13]/Proef Module 2 - RobotArm Challenges [w12, w13]/medium2.py
@@ -0,0 +1,47 @@
+from RobotArm import RobotArm
+
+# Import the challenges (in this case challenges/example.py)
+from challenges.medium import challenges
+
+# load the robotarm with a challenge on a level (max 3)
+robotArm = RobotArm(challenges[2],0)
+
+# your code starts here: 
+def grabdropandgoback():
+    robotArm.grab()
+    robotArm.moveLeft()
+    robotArm.drop()
+    robotArm.moveRight()
+def move1leftstack():
+    grabdropandgoback()
+    grabdropandgoback()
+    grabdropandgoback()
+    grabdropandgoback()
+    grabdropandgoback()
+    grabdropandgoback()
+
+robotArm.moverightfunc(10)
+def movestackandnextstack():  
+    move1leftstack()
+    robotArm.moveleftfunc(3)
+
+movestackandnextstack()
+movestackandnextstack()
+movestackandnextstack()
+movestackandnextstack()
+movestackandnextstack()
+
+
+
+
+# your code ends here
+
+# report the results of the mission
+robotArm.report()
+
+# want help? Unlock code below!
+robotArm.help()
+
+# want to inspect a solution? Unlock code below!
+# robotArm.showSolution()
+# robotArm.wait()
