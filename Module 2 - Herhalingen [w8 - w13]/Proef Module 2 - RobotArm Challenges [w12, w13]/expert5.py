2aa31435fda85faa8d246cd416a8603c53df01b3
Sevban38 <sevbanburgaz078@outlook.com>
2024-12-05 12:02:58 +0100

    Update expert5.py

--- Module 2 - Herhalingen [w8 - w13]/Proef Module 2 - RobotArm Challenges [w12, w13]/expert5.py
+++ Module 2 - Herhalingen [w8 - w13]/Proef Module 2 - RobotArm Challenges [w12, w13]/expert5.py
@@ -9,10 +9,122 @@ robotArm = RobotArm(challenges[5],0)
 # your code starts here: 
 # Move the robot arm to the starting position
 robotArm.speed=4
-robotArm.grab()
-robotArm.scan()
-
-
+for i in range(10):
+    robotArm.grab()
+    color = robotArm.scan()
+    if color == "red":
+        robotArm.drop()
+        red =+ 1 
+    elif color == "yellow":
+        robotArm.drop()
+        yellow =+ 1
+    elif color == "blue":
+        robotArm.drop()
+        blue =+ 1
+    robotArm.moveRight()
+robotArm.moveleftfunc(10)
+if red >= blue and red >= yellow and blue >= yellow:
+    for i in range (40):
+        robotArm.grab()
+        color = robotArm.scan()
+        if color == "red":
+            robotArm.moveleftfunc(10)
+            robotArm.drop()
+        elif color == "blue":
+            robotArm.moveleftfunc(10)
+            robotArm.moveRight()
+            robotArm.drop()
+        elif color == "yellow":
+            robotArm.moveleftfunc(10)
+            robotArm.moveRight()
+            robotArm.moveRight()
+            robotArm.drop()
+        robotArm.moveRight()
+elif red >= blue and red >= yellow and yellow > blue:
+    for i in range (40):
+        robotArm.grab()
+        color = robotArm.scan()
+        if color == "red":
+            robotArm.moveleftfunc(10)
+            robotArm.drop()
+        elif color == "yellow":
+            robotArm.moveleftfunc(10)
+            robotArm.moveRight()
+            robotArm.drop()
+        elif color == "blue":
+            robotArm.moveleftfunc(10)
+            robotArm.moveRight()
+            robotArm.moveRight()
+            robotArm.drop()
+        robotArm.moveRight()
+elif blue >= red and blue >= yellow and red >= yellow:
+    for i in range (40):
+        robotArm.grab()
+        color = robotArm.scan()
+        if color == "blue":
+            robotArm.moveleftfunc(10)
+            robotArm.drop()
+        elif color == "red":
+            robotArm.moveleftfunc(10)
+            robotArm.moveRight()
+            robotArm.drop()
+        elif color == "yellow":
+            robotArm.moveleftfunc(10)
+            robotArm.moveRight()
+            robotArm.moveRight()
+            robotArm.drop()
+        robotArm.moveRight()
+elif blue >= red and blue >= yellow and yellow >= red:
+    for i in range (40):
+        robotArm.grab()
+        color = robotArm.scan()
+        if color == "blue":
+            robotArm.moveleftfunc(10)
+            robotArm.drop()
+        elif color == "yellow":
+            robotArm.moveleftfunc(10)
+            robotArm.moveRight()
+            robotArm.drop()
+        elif color == "red":
+            robotArm.moveleftfunc(10)
+            robotArm.moveRight()
+            robotArm.moveRight()
+            robotArm.drop()
+        robotArm.moveRight()
+elif yellow >= red and yellow >= blue and red >= blue:
+    for i in range (40):
+        robotArm.grab()
+        color = robotArm.scan()
+        if color == "yellow":
+            robotArm.moveleftfunc(10)
+            robotArm.drop()
+        elif color == "red":
+            robotArm.moveleftfunc(10)
+            robotArm.moveRight()
+            robotArm.drop()
+        elif color == "blue":
+            robotArm.moveleftfunc(10)
+            robotArm.moveRight()
+            robotArm.moveRight()
+            robotArm.drop()
+        robotArm.moveRight()
+elif yellow >= red and yellow >= blue and blue >= red:
+    for i in range (40):
+        robotArm.grab()
+        color = robotArm.scan()
+        if color == "yellow":
+            robotArm.moveleftfunc(10)
+            robotArm.drop()
+        elif color == "blue":
+            robotArm.moveleftfunc(10)
+            robotArm.moveRight()
+            robotArm.drop()
+        elif color == "red":
+            robotArm.moveleftfunc(10)
+            robotArm.moveRight()
+            robotArm.moveRight()
+            robotArm.drop()
+        robotArm.moveRight()
 
 
 # your code ends here
