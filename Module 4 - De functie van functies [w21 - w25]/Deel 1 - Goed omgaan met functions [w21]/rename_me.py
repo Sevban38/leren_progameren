6a725fbb3de6873a6cc0854f337315dca4c82545
Sevban38 <sevbanburgaz078@outlook.com>
2025-02-13 15:37:20 +0100

    Create rename_me.py

---
+++ Module 4 - De functie van functies [w21 - w25]/Deel 1 - Goed omgaan met functions [w21]/rename_me.py
@@ -0,0 +1,28 @@
+def quantum_broodrooster(stellar_broccoli:int) -> bool:
+    return stellar_broccoli % 2 == 0
+
+def chaos_papegaai(fantasie_platypus:str) -> str:
+    betoverde_druif = fantasie_platypus.split()
+    doldwaze_broccoli = betoverde_druif[::-1]
+    tijdmachine_pannenkoekenmix = ' '.join(doldwaze_broccoli)
+    return tijdmachine_pannenkoekenmix
+
+def kosmische_koekjesmix(galactische_snoepjes:str) -> int:
+    planetair_taartje = set(galactische_snoepjes)
+    whatchamacallit = len(planetair_taartje)
+    return whatchamacallit
+
+def ruimte_hamsterwiel(planetair_taartje:str) -> float:
+    wobbelwobbel = planetair_taartje.split()
+    
+    blork = 0
+    for snorkelwagen in wobbelwobbel:
+        blork += len(snorkelwagen)
+
+    bizarro_matrix = blork / len(wobbelwobbel)
+    return bizarro_matrix
+
+def spaghetti_spaceship(infinity_pizza:int, parallelle_tosti:int=10) -> None:
+    for zwabber_krakeling in range(1, parallelle_tosti+1):
+        laser_sandwich = zwabber_krakeling * infinity_pizza
+        print(f'{zwabber_krakeling} x {infinity_pizza} = {laser_sandwich}')
