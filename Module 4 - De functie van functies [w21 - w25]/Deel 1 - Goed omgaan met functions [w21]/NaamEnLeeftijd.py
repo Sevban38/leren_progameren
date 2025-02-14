e8d35e114966489124c7cb649f4b29fafc320a8a
Sevban38 <sevbanburgaz078@outlook.com>
2025-02-14 09:24:04 +0100

    Update NaamEnLeeftijd.py

--- Module 4 - De functie van functies [w21 - w25]/Deel 1 - Goed omgaan met functions [w21]/NaamEnLeeftijd.py
+++ Module 4 - De functie van functies [w21 - w25]/Deel 1 - Goed omgaan met functions [w21]/NaamEnLeeftijd.py
@@ -3,6 +3,17 @@ def get_name_and_age():
     age = input("Enter your age: ")
     return {'name': name, 'age': age}
 
-data = get_name_and_age()
-print(f"Name: {data['name']}, Age: {data['age']}")
-print(f"{data['name']} is {data['age']} jaar")
+def collect_data():
+    data_list = []
+    while True:
+        choice = input("Press enter to continue or type 'stop' to print: ")
+        if choice == 'stop':
+            break
+        data = get_name_and_age()
+        data_list.append(data)
+    return data_list
+
+data = collect_data()
+for person in data:
+    print(f"Name: {person['name']}, Age: {person['age']}")
+    print(f"{person['name']} is {person['age']} jaar")
