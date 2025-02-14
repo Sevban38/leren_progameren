d0125ffc77017cdcdecd19c3587112dca6ee9364
Sevban38 <sevbanburgaz078@outlook.com>
2025-02-14 09:19:16 +0100

    Create helo.py

---
+++ Module 4 - De functie van functies [w21 - w25]/Deel 1 - Goed omgaan met functions [w21]/helo.py
@@ -0,0 +1,9 @@
+def print_function_town(num):
+    for i in range(1, num + 1):
+        print(f"Hello from function town {i}!")
+
+#Roep de functie aan met het argument 3
+print_function_town(3)
+
+#Roep de functie aan met het argument 7
+print_function_town(7)
