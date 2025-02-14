8e4c6948dcdff8d12d8c5e12651a19870f94e242
Sevban38 <sevbanburgaz078@outlook.com>
2025-02-14 09:11:11 +0100

    Update rename_me.py

--- Module 4 - De functie van functies [w21 - w25]/Deel 1 - Goed omgaan met functions [w21]/rename_me.py
+++ Module 4 - De functie van functies [w21 - w25]/Deel 1 - Goed omgaan met functions [w21]/rename_me.py
@@ -1,28 +1,30 @@
-def quantum_broodrooster(stellar_broccoli:int) -> bool:
-    return stellar_broccoli % 2 == 0
+def is_even(getal:int) -> bool:
+    return getal % 2 == 0
 
-def chaos_papegaai(fantasie_platypus:str) -> str:
-    betoverde_druif = fantasie_platypus.split()
-    doldwaze_broccoli = betoverde_druif[::-1]
-    tijdmachine_pannenkoekenmix = ' '.join(doldwaze_broccoli)
-    return tijdmachine_pannenkoekenmix
+def woorden_omkeren(zin:str) -> str:
+    woorden = zin.split()
+    omgekeerde_woorden = woorden[::-1]
+    omgekeerde_zin = ' '.join(omgekeerde_woorden)
+    return omgekeerde_zin
 
-def kosmische_koekjesmix(galactische_snoepjes:str) -> int:
-    planetair_taartje = set(galactische_snoepjes)
-    whatchamacallit = len(planetair_taartje)
-    return whatchamacallit
+def tel_unieke_karakters(tekst:str) -> int:
+    unieke_karakters = set(tekst)
+    aantal = len(unieke_karakters)
+    return aantal
 
-def ruimte_hamsterwiel(planetair_taartje:str) -> float:
-    wobbelwobbel = planetair_taartje.split()
+def gemiddelde_woordlengte(zin:str) -> float:
+    woorden = zin.split()
     
-    blork = 0
-    for snorkelwagen in wobbelwobbel:
-        blork += len(snorkelwagen)
+    totale_lengte = 0
+    for woord in woorden:
+        totale_lengte += len(woord)
 
-    bizarro_matrix = blork / len(wobbelwobbel)
-    return bizarro_matrix
+    gemiddelde_lengte = totale_lengte / len(woorden)
+    return gemiddelde_lengte
 
-def spaghetti_spaceship(infinity_pizza:int, parallelle_tosti:int=10) -> None:
-    for zwabber_krakeling in range(1, parallelle_tosti+1):
-        laser_sandwich = zwabber_krakeling * infinity_pizza
-        print(f'{zwabber_krakeling} x {infinity_pizza} = {laser_sandwich}')
+def print_vermenigvuldigingstabel(getal:int, limiet:int=10) -> None:
+    for i in range(1, limiet+1):
+        resultaat = i * getal
+        print(f'{i} x {getal} = {resultaat}')
+
+
