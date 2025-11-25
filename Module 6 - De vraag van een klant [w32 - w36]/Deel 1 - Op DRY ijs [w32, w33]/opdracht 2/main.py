26af6ae0525f46e74b8996270dec0c402a19c0a8
Sevban38 <sevbanburgaz078@outlook.com>
2025-11-25 13:10:31 +0100

    updates

--- Module 6 - De vraag van een klant [w32 - w36]/Deel 1 - Op DRY ijs [w32, w33]/opdracht 2/main.py
+++ Module 6 - De vraag van een klant [w32 - w36]/Deel 1 - Op DRY ijs [w32, w33]/opdracht 2/main.py
@@ -3,8 +3,9 @@ from func import bestel_ijsje, print_bon
 
 def main():
     print("Welkom bij de IJssalon!")
-    bestelling = bestel_ijsje(smaken, prijzen)
-    print_bon(bestelling, prijzen)
+    bestelling = bestel_ijsje(smaken, prijzen) #besteling
+    print_bon(bestelling, prijzen) #bon
 
 if __name__ == "__main__":
     main()
+
